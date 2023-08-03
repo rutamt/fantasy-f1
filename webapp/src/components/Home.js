daded865ebfded8d6534568cb8fea4f7498e4010
rutamt <95180713+rutamt@users.noreply.github.com>
2023-08-03 10:25:28 -0400

    Added an example card in home page

--- webapp/src/components/Home.js
+++ webapp/src/components/Home.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { Button, Modal } from "antd";
+import { Modal, Card, Image, Typography, Space, Row, Col } from "antd";
+import { EditOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -25,6 +28,37 @@ function Home() {
         <p>Some contents...</p>
         <p>Some contents...</p>
       </Modal>
+
+      <Card
+        title="Team 1"
+        extra={"15M"}
+        style={{
+          width: 300,
+        }}
+        actions={[<EditOutlined key="edit" />]}
+      >
+        {" "}
+        <Row>
+          <Col span={12}>
+            <Image
+              width={100}
+              height={100}
+              src="https://www.stickers-garage.com/imgtransform.php?f=1&filename=https://www.stickers-garage.com/resize/720x720/zc/3/f/0/src/sites/stickers/files/products/ma0515.png"
+            />
+          </Col>
+          <Col span={12}>
+            <Image
+              width={100}
+              height={100}
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Lando_Norris_McLaren_driver_number.svg/1200px-Lando_Norris_McLaren_driver_number.svg.png"
+            />
+          </Col>
+        </Row>
+        <Space direction="vertical" size={16}>
+          <Image src="https://e00-especiales-marca.uecdn.es/motor/images/formula1/escuderias/2023/gral/coche-mercedes-mediasombra.png"></Image>
+          <Title level={5}>Mercedes</Title>
+        </Space>
+      </Card>
     </div>
   );
 }
